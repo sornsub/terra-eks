ea05ee843162fd844bfb993766b6dec8e789b746
sornsub <150174671+sornsub@users.noreply.github.com>
2026-03-21 22:06:44 +0700

    Add Repo

---
+++ eks-cluster.tf
@@ -0,0 +1,44 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "21.0"
+  หenable_cluster_creator_admin_permissions = true
+  cluster_name    = local.cluster_name
+  cluster_version = "1.31"
+
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
+  cluster_endpoint_public_access = true
+
+  cluster_addons = {
+    coredns    = {}
+    kube-proxy = {}
+    vpc-cni    = {}
+  }
+
+  eks_managed_node_group_defaults = {
+    ami_type = "AL2023_x86_64_STANDARD"
+
+  }
+
+  eks_managed_node_groups = {
+    one = {
+      name = "node-group-1"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+    }
+
+    two = {
+      name = "node-group-2"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+    }
+  }
+}
---
+++ terraform.tf
@@ -0,0 +1,45 @@
+terraform {
+  
+  required_version = ">= 1.7, < 2.0"
+
+  required_providers {
+    # create manage aws ex. vpc ec2
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.0"
+    }
+    # create random pass
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6.0"
+    }
+    # manage certificate TLS/SSL
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.5"
+    }
+    # create manages vm with cloud
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.3.3"
+    }
+    # create manages kubernetes
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.27"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.12"
+    }
+
+  }
+
+  backend "s3" {
+    bucket = "terra-eks-demo-storage"
+    key    = "state/terraform.tfstate"
+    region = "us-east-1"
+  }
+
+}
